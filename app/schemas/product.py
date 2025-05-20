1735eb443404a73ec2084213df16a19c3de938d5
Tri Susilo <trisusilo.developer@gmail.com>
2025-05-20 12:17:48 +0700

    change the class product response

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, StrictFloat
 
 
 class ProductBase(BaseModel):
-    name : str = Field(..., min_length=1, max_length=100)
+    name : str = Field(..., min_length=1, max_length=100, description="Nama produk")
     description: Optional[str] = Field(None, min_length=1)
-    price: float = Field(..., gt=0)
+    price: StrictFloat = Field(..., gt=0)
     stock: Optional[int] = Field(0, ge=0)
 
 class ProductCreate(ProductBase):
@@ -18,8 +18,11 @@ class ProductUpdate(ProductBase):
     price: Optional[float] = Field(None, gt=0)
     stock: Optional[int] = Field(None, ge=0)
 
-class ProductResponse(ProductBase):
+class ProductResponse(BaseModel):
     id: int
+    name: str
+    price: float
+    stock:int
     created_at: datetime
     updated_at: datetime
 
